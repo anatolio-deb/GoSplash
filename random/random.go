07e357a14df0417b6b4b67fc5f8ca0a83882cf19
anatolio-deb <nikiforova693@gmail.com>
2023-06-04 09:29:20 +0000

    fix

--- random/random.go
+++ random/random.go
@@ -1,37 +1,14 @@
 package random
 
-// type Links struct {
-// 	Self      string `json:"self"`
-// 	HTML      string `json:"html"`
-// 	Photos    string `json:"photos"`
-// 	Likes     string `json:"likes"`
-// 	Portfolio string `json:"portfolio"`
-// }
-// type User struct {
-// 	Id                string `json:"id"`
-// 	UpdatedAt         string `json:"updated_at"`
-// 	Username          string `json:"username"`
-// 	Name              string `json:"name"`
-// 	PortfolioUrl      string `json:"portfolio_url"`
-// 	Bio               string `json:"bio"`
-// 	Location          string `json:"location"`
-// 	TotalLikes        string `json:"total_likes"`
-// 	TotalPhotos       string `json:"total_photos"`
-// 	TotalCollections  string `json:"total_collections"`
-// 	InstagramUsername string `json:"instagram_username"`
-// 	TwitterUsername   string `json:"twitter_username"`
-// 	Links             Links
-// }
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+)
 
-//	type Collection struct {
-//		ID              string `json:"id"`
-//		Title           string `json:"title"`
-//		PublishedAt     string `json:"published_at"`
-//		LastCollectedAt string `json:"last_collected_at"`
-//		UpdatedAt       string `json:"updated_at"`
-//		CoverPhoto      Photo `json:"cover_photo"`
-//		User            User
-//	}
 type Links struct {
 	Self             string `json:"self"`
 	HTML             string `json:"html"`
@@ -43,3 +20,66 @@ type Photo struct {
 	ID    string `json:"id"`
 	Links Links  `json:"links"`
 }
+
+func Get(topicIDs []string) (Photo, error) {
+	var p Photo
+	url, err := url.Parse("https://api.unsplash.com")
+	if err != nil {
+		return p, err
+	}
+	url.Path = "/photos/random/"
+	params := url.Query()
+	params.Add("content_filter", "high")
+	params.Add("orientation", "landscape")
+	if len(topicIDs) > 0 {
+		params.Add("topics", strings.Join(topicIDs, ","))
+	}
+	url.RawQuery = params.Encode()
+
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodGet, url.String(), strings.NewReader(params.Encode()))
+
+	if err != nil {
+		return p, err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Client-ID %s", ""))
+
+	if err != nil {
+		return p, err
+	}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return p, err
+	}
+
+	b, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		return p, err
+	}
+
+	return p, json.Unmarshal(b, &p)
+}
+
+// func GetDownloadURL(url string) (string, error) {
+// 	var r DownloadURL
+// 	client := &http.Client{}
+// 	req, err := http.NewRequest(http.MethodGet, url, nil)
+// 	if err != nil {
+// 		return r.URL, err
+// 	}
+// 	req.Header.Add("Authorization", fmt.Sprintf("Client-ID %s", ""))
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+// 		return r.URL, err
+// 	}
+// 	b, err := io.ReadAll(resp.Body)
+// 	if err != nil {
+// 		return r.URL, err
+// 	}
+
+// 	return r.URL, json.Unmarshal(b, &r)
+// }
